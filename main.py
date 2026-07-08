48230fd2c6f3add98797c77fb19ba637445c0382
24f2007664 <24f2007664@ds.iitm.study.ac.in>
2026-07-08 20:36:06 +0530

    Fix CORS

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ app = FastAPI()
 # Enable CORS
 # ------------------------
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
